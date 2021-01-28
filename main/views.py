4ff1ab6fc63e1b8d9fe7461da07a84a74665e414
ridham nagar <ridham6299@gmail.com>
2021-01-28 13:46:41 +0530

    second commit

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tutorials
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from .forms import NewUserForm
 # Create your views here.
 def homepage(request):
     return render(request=request,
@@ -12,20 +13,18 @@ def homepage(request):
 
 def register(request):
         if request.method == "POST":
-            form = UserCreationForm(request.POST)
+            form = NewUserForm(request.POST)
             if form.is_valid():
                 user = form.save()
-                usename= form.cleaned_data.get('username')
+                username= form.cleaned_data.get('username')
                 messages.success(request, f"New account created: {username}")
                 login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
                 return redirect("main:homepage")
             else:
                 for msg in form.error_messages:
                     messages.error(request, f"{msg}: {form.error_messages[msg]}")
-
-
-
-        form=UserCreationForm
+        form=NewUserForm
         return render(request=request,
         template_name="main/register.html",
         context={"form":form})
@@ -36,20 +35,24 @@ def logout_request(request):
     return redirect("main:homepage")
 
 def login_request(request):
-        #if request.method == "POST":
-        #    form = AuthenticationForm(request, data=request.POST)
-        #    if form.is_valid():
-#               usename= form.cleaned_data.get('username')
-            #   password= form.cleaned_data.get('password')
-        #        user= authenticate(username=username, password=password)
-            #    if user is not None:
-            #        login(request, user)
-            #        messages.info(request, f"You are now logged in as {username}")
-            #        return redirect("main:homepage")
-            #    else:
-            #        messages.error(request, f"{msg}: {form.error_messages[msg]}")
-        #    else:
-            #    messages.error(request, f"{msg}: {form.error_messages[msg]}")
-    form= AuthenticationForm()
-    return render(request,"main/login.html",
-    {"form":form})
+        if request.method == "POST":
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+              username= form.cleaned_data.get('username')
+              password= form.cleaned_data.get('password')
+              user= authenticate(username=username, password=password)
+              if user is not None:
+                   login(request, user)
+                   messages.info(request, f"You are now logged in as {username}")
+                   return redirect("main:homepage")
+              else:
+                  messages.error(request, f"{msg}: {form.error_messages[msg]}")
+                  print("error")
+            else:
+                #messages.error(request, f"{msg}: {form.error_messages[msg]}")
+                print("error")
+
+        form= AuthenticationForm()
+        return render(request,
+                     "main/login.html",
+                     {"form":form})
